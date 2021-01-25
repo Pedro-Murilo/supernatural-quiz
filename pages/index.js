a53e779bbd9b5afbe766f34b88a32049d53a4c7a
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-25 17:32:58 -0300

    add: added all files with create-next-app

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,102 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
 
 const Title = styled.h1`
   font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #333;
+  background-color: #222;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1,
+  h2,
+  h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+            <Widget.Header>
+              <h1>Anime Quiz</h1>
+            </Widget.Header>
+          <Widget.Content>
+              <p>What you know about animes?</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+              <h2>Put your quiz here</h2>
+              <p>Lorem ipsum dolor sit</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
