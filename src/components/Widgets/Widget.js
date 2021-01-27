bf76803e40833a9ea065c75d3effe7bf57234c69
Pedro Murilo <pedromurilocalvi@gmail.com>
2021-01-27 12:18:21 -0300

    feat: created quiz page and created quiz funcionality

--- src/components/Widgets/Widget.js
+++ src/components/Widgets/Widget.js
@@ -3,20 +3,16 @@ import styled from 'styled-components';
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid #333;
-  background-color: #555;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
   border-radius: 4px;
   overflow: hidden;
-
-  h1,
-  h2,
-  h3 {
+  h1, h2, h3 {
     font-size: 16px;
     font-weight: 700;
     line-height: 1;
     margin-bottom: 0;
   }
-
   p {
     font-size: 14px;
     font-weight: 400;
@@ -30,7 +26,7 @@ Widget.Header = styled.header`
   align-items: center;
   padding: 18px 32px;
   background-color: ${({ theme }) => theme.colors.primary};
-
+  
   * {
     margin: 0;
   }
@@ -41,14 +37,30 @@ Widget.Content = styled.div`
   & > *:first-child {
     margin-top: 0;
   }
-
   & > *:last-child {
     margin-bottom: 0;
   }
-
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+Widget.Topic = styled.a`
+  outline: 0;
+  text-decoration: none;
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => `${theme.colors.primary}40`};
+  padding: 10px 15px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.borderRadius};
+  transition: .3s;
+  display: block;
+  
+  &:hover,
+  &:focus {
+    opacity: .5;
   }
 `;
 
